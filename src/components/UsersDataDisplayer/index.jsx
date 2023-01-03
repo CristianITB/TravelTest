b60f00c9eb988b7aa3ed909dac8c414a4bd96667
Cristian <cristian,jimenez.7e5@itb.cat>
2023-01-03 17:12:18 +0100

    part II done

--- src/components/UsersDataDisplayer/index.jsx
+++ src/components/UsersDataDisplayer/index.jsx
@@ -1,10 +1,29 @@
 import SingleUserDataDisplayer from '../SingleUserDataDisplayer'
-import { Frame } from './styles'
+import { Frame, InsertUsers, NumberOfUsers, AddUsersButton } from './styles'
+import { useState } from 'react'
 
 export const UsersDataDisplayer = ({ usersData }) => {
+  const [numberOfUsers, setNumberOfUsers] = useState(0)
+  const [usersDataListState, setUsersDataListState] = useState([])
+
+  const modifyUsersList = () => {
+    if (numberOfUsers < usersData.length) {
+      setNumberOfUsers(numberOfUsers + 1)
+      usersDataListState.push(usersData[numberOfUsers])
+      setUsersDataListState(usersDataListState)
+    } else if (numberOfUsers === usersData.length) {
+      setNumberOfUsers(0)
+      setUsersDataListState([])
+    }
+  }
+
   return (
     <Frame>
-      {usersData.map((user, index) => {
+      <InsertUsers>
+        <NumberOfUsers>Number of current users: {numberOfUsers}...Do you want more?</NumberOfUsers>
+        <AddUsersButton onClick={modifyUsersList}>Add User</AddUsersButton>
+      </InsertUsers>
+      {usersDataListState.map((user, index) => {
         return (
           <SingleUserDataDisplayer key={index} userFirstName={user.name.first} userLastName={user.name.last} userEmail={user.email} userImage={user.picture.medium} />
         )
--- src/components/UsersDataDisplayer/styles.js
+++ src/components/UsersDataDisplayer/styles.js
@@ -1,6 +1,22 @@
 import styled from '@emotion/styled'
+import { withTheme } from '../../helpers/styles.helper'
 
 export const Frame = styled.div`
   display: flex;
   flex-direction: column;
+`
+
+export const InsertUsers = styled.div`
+  display: flex;
+  flex-direction: row;
+`
+
+export const NumberOfUsers = styled.p`
+  ${withTheme('margin', 'sizes.rem15')};
+`
+
+export const AddUsersButton = styled.button`
+  ${withTheme('padding', 'sizes.rem04')};
+  ${withTheme('height', 'sizes.rem2')};
+  ${withTheme('margin-top', 'sizes.rem1')};
 `
